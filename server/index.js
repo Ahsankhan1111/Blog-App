35157f0794508e99519542dd4780548f1f2f249a
Ahsankhan1111 <ahsan.t@blurredego.com>
2024-01-21 21:23:06 +0500

    connect to mongoDb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+mongoose.connect(process.env.mongo_url).then(()=>{
+    console.log("database connected");
+}).catch((err)=>{
+        console.log(err);
+    }
+)
 const app = express();
 
 app.listen(3000,()=>{
